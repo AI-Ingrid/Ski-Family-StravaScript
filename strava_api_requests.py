6f2387cad0469059ab177631a6b9e73d0da4a859
Ingrid Kolderup <ikolderu@cisco.com>
2024-12-10 10:37:18 +0100

    add functionality that handles automatic refreshing of access token when needed

--- strava_api_requests.py
+++ strava_api_requests.py
@@ -1,5 +1,5 @@
 import os
-import json
+import time
 import requests
 
 from utils import store_activities_with_metadata
@@ -17,6 +17,7 @@ class StravaAPI:
         self.auth_url = os.getenv('AUTH_URL')
         self.refresh_token = os.getenv('REFRESH_TOKEN')
         self.access_token = None
+        self.token_expires_at = 0
 
     def get_refresh_token(self):
         try:
@@ -28,9 +29,14 @@ class StravaAPI:
             }
             response = requests.post(self.token_url, data=payload)
             response.raise_for_status()
-            self.refresh_token = response.json().get('refresh_token')
+            data = response.json()
+
+            self.refresh_token = data.get('refresh_token')
+            self.token_expires_at = data.get('expires_at', 0)
+            
             print("Refresh Token obtained successfully.")
             return self.refresh_token
+        
         except requests.exceptions.RequestException as e:
             print(f"Error obtaining refresh token: {e}")
 
@@ -39,22 +45,32 @@ class StravaAPI:
             print("Refresh token is not set. Cannot obtain access token.")
             return
 
-        try:
-            payload = {
-                'client_id': self.client_id,
-                'client_secret': self.client_secret,
-                'grant_type': 'refresh_token',
-                'refresh_token': self.refresh_token
-            }
-            response = requests.post(self.token_url, data=payload)
-            response.raise_for_status()
-            self.access_token = response.json().get('access_token')
-            print("Access Token obtained successfully.")
-            return self.access_token
-        except requests.exceptions.RequestException as e:
-            print(f"Error obtaining access token: {e}")
+        if time.time() >= self.token_expires_at:
+            try:
+                payload = {
+                    'client_id': self.client_id,
+                    'client_secret': self.client_secret,
+                    'grant_type': 'refresh_token',
+                    'refresh_token': self.refresh_token
+                }
+                response = requests.post(self.token_url, data=payload)
+                response.raise_for_status()
+                data = response.json()
 
+                self.access_token = data.get('access_token')
+                self.token_expires_at = data.get('expires_at', 0)
+
+                print("Access Token obtained successfully.")
+                return self.access_token
+            except requests.exceptions.RequestException as e:
+                print(f"Error obtaining access token: {e}")
+        else:
+            print("Access token is still valid.")
+            return self.access_token
+        
     def get_new_club_activities_and_store_them(self, after, per_page=200, page=1, last_activity_number=0, filename='data/activities.csv'):
+        self.get_access_token()
+
         if not self.access_token:
             print("Access token is not set. Cannot fetch club activities.")
             return []
