0ba0b136c173c99b5ffd6442884137ad60916f16
Ingrid Kolderup <ikolderu@cisco.com>
2024-12-10 12:50:35 +0100

    squas git add command to fix bug of having two of these lines

--- push_to_github.py
+++ push_to_github.py
@@ -9,15 +9,15 @@ def commit_and_push_changes(abs_path_repo):
     
     try:
         # Add changes to git
-        subprocess.run(["git", "add", path_distance_plot], check=True)
-        subprocess.run(["git", "add", path_elevation_plot], check=True)
+        subprocess.run(["git", "add", path_distance_plot, path_elevation_plot], check=True)
 
         # Commit changes
         subprocess.run(["git", "commit", "-m", "Update bar charts"], check=True)
 
         # Push changes
-        subprocess.run(["git", "push"], check=True)  # Adjust branch name if necessary
+        subprocess.run(["git", "push"], check=True) 
 
         print("Changes pushed to GitHub successfully.")
+    
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
