e98c83fa07fe0df053d6e0fc96b04d5fb1c4fc4b
Ingrid Kolderup <ikolderu@cisco.com>
2024-12-10 10:16:44 +0100

    add abs path to emoji files and create a get_emoji_images func

--- main.py
+++ main.py
@@ -19,18 +19,26 @@ def fetch_activities_after_date(date, strava_api, path_to_activity_data):
     all_activities = get_all_stored_activities(filename=path_to_activity_data)
     return all_activities
 
+def get_emoji_images(abs_path):
+    emoji_files = ['emojis/1st-place.png', 'emojis/2nd-place.png', 'emojis/3rd-place.png', 
+                   'emojis/troll.png', 'emojis/troll.png', 'emojis/troll.png', 'emojis/troll.png']
+    
+    # Prepend abs_path to each file name
+    return [f"{abs_path}/{emoji}" for emoji in emoji_files]
+
 
 def main():
     strava_api = connect_to_strava_api()
 
     first_of_november = int(datetime(2024, 11, 1).timestamp())
-    path_to_activity_data = os.getenv('ABS_PATH_REPO') + '/data/activities.csv'
+    abs_path = os.getenv('ABS_PATH_REPO')
+    path_to_activity_data = abs_path + '/data/activities.csv'
+    
     activities = fetch_activities_after_date(first_of_november, strava_api, path_to_activity_data)
 
 
     if activities:
-        emoji_images = ['emojis/1st-place.png', 'emojis/2nd-place.png', 'emojis/3rd-place.png', 
-                        'emojis/troll.png', 'emojis/troll.png', 'emojis/troll.png', 'emojis/troll.png']
+        emoji_images = get_emoji_images(abs_path)
         plot_activity_charts(activities, 'NordicSki', emoji_images)
         commit_and_push_changes()
 
