2b9ca4c4ccde7c9a016cfc1f8f69427e02a58744
ikolderu <ikolderu@cisco.com>
2024-12-16 12:41:32 +0100

    Update ssh agent script with some variable name changes

--- start_ssh_agent.sh
+++ start_ssh_agent.sh
@@ -10,15 +10,15 @@ fi
 source "$1"
 
 # Use the environment variables
-SSH_PATH="${SSH_KEY_PATH}"
-SSH_PRIVATE_KEY="${SSH_KEY_PATH}/id_rsa"
+SSH_AGENT_ENV="${SSH_KEY_PATH}"
+SSH_KEY="${SSH_KEY_PATH}/id_rsa"
 
 # Start the SSH agent
 eval "$(ssh-agent -s)"
 
 # Add the SSH key
-ssh-add SSH_PRIVAT_KEY
+ssh-add SSH_KEY
 
 # Save the SSH agent information
-echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" > SSH_PATH
-echo "export SSH_AGENT_PID=$SSH_AGENT_PID" >> SSH_PATH
+echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" > SSH_AGENT_ENV
+echo "export SSH_AGENT_PID=$SSH_AGENT_PID" >> SSH_AGENT_ENV
